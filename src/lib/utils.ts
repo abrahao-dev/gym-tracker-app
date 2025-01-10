416c95d327da7fc6bfbc4b147fe0575a628ec33d
abrahao-dev <matheus.alvares23@outlook.com>
2025-01-10 14:27:19 -0300

    Update only frontend working

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export const calculateProgress = (value: number, total: number): number => {
-  return Number(((value / total) * 100).toFixed(6))
-}
-
-export const formatDate = (date: Date): string => {
-  return date.toISOString().split('T')[0]
-}
